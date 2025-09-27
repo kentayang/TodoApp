bdf2592212797da0442fc67768dd8fdd70e5b139
Gian Aguilar <gian.aguilar89@gmail.com>
2025-09-27 15:02:11 -0500

    Agregando cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import tasksRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Prueba
 app.get("/", (req, res) => {
